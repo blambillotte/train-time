91830becc6bca1f9a2d45057adb82500d794fdfc
Ben Lambillotte <ben.lambillotte@gmail.com>
2017-09-30 16:14:09 -0700

    Added update and delete handeling the trains

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -18,6 +18,8 @@ var database = firebase.database();
 
 var submitBtn = $('#submit-btn');
 var trainTable = $('#train-table');
+var editModal = $('#train-modal');
+var modalCloseBtn = $('#modal-close');
 
 var trainName = $('#train-name');
 var trainDestination = $('#destination');
@@ -44,7 +46,7 @@ submitBtn.on('click', function(event) {
 
 function saveToDB() {
 
-  database.ref().push({
+  database.ref('trains/').push({
     trainName: trainName.val().trim(),
     trainDestination: trainDestination.val().trim(),
     trainStartTime: trainStartTime.val().trim(),
@@ -63,14 +65,15 @@ function clearEntries() {
 }
 
 // Firebase watcher + initial loader HINT: This code behaves similarly to .on("value")
-database.ref().on("child_added", function(childSnapshot) {
+database.ref('trains/').on("child_added", function(childSnapshot) {
 
 
   // Log everything that's coming out of snapshot
-  console.log(childSnapshot.val().trainName);
-  console.log(childSnapshot.val().trainDestination);
-  console.log(childSnapshot.val().trainStartTime);
-  console.log(childSnapshot.val().trainFrequency);
+  // console.log(childSnapshot.val().trainName);
+  // console.log(childSnapshot.val().trainDestination);
+  // console.log(childSnapshot.val().trainStartTime);
+  // console.log(childSnapshot.val().trainFrequency);
+  console.log(childSnapshot.key);
 
   //Get the value for Months Worked
   //Start date into a Moment Object
@@ -88,13 +91,28 @@ database.ref().on("child_added", function(childSnapshot) {
   var nextArrivalTd = $('<td>').html(timeCalc.nextArrivalTime);
   var minutesAwayTd = $('<td>').html(timeCalc.tMinutesTillTrain);
 
-  console.log(timeCalc);
+
+  var deleteBtn = $('<span>');
+  deleteBtn.attr({
+    class: 'glyphicon glyphicon-minus-sign',
+    id: childSnapshot.key,
+    'data-action': 'delete'
+    });
+
+  var editBtn = $('<span>');
+  editBtn.attr({
+    class: 'glyphicon glyphicon-pencil',
+    id: childSnapshot.key,
+    'data-action': 'edit'
+    });
+
+  var buttonsTd = $('<td>').append(editBtn, deleteBtn);
+
   //full list of items to the well
-  console.log(nameTd);
-  newRow.append(nameTd, destinationTd, frequencyTd, nextArrivalTd, minutesAwayTd);
-  //  "<td>" + 'placeholder' + "</td>" + "<td>" + 'placeholder' + "</td>"
-  console.log(newRow);
+  newRow.append(nameTd, destinationTd, frequencyTd, nextArrivalTd, minutesAwayTd, buttonsTd);
+  // console.log(newRow);
   trainTable.append(newRow);
+
 
 // Handle the errors
 }, function(errorObject) {
@@ -106,27 +124,26 @@ function getNextTrainTime(frequencyVal, startTimeVal) {
 
     // First Time (pushed back 1 year to make sure it comes before current time)
     var firstTimeConverted = moment(startTimeVal, "hh:mm").subtract(1, "years");
-    console.log(firstTimeConverted);
+    // console.log(firstTimeConverted);
 
     // Current Time
     var currentTime = moment();
-    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+    // console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
 
     // Difference between the times
     var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-    console.log("DIFFERENCE IN TIME: " + diffTime);
+    // console.log("DIFFERENCE IN TIME: " + diffTime);
 
     // Time apart (remainder)
     var tRemainder = diffTime % frequencyVal;
-    console.log(tRemainder);
 
     // Minute Until Train
     var tMinutesTillTrain = frequencyVal - tRemainder;
-    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+    // console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
 
     // Next Train
     var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+    // console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
     var nextArrivalTime = moment(nextTrain).format("hh:mm A");
 
     return {
@@ -152,3 +169,81 @@ function checkform(form) {
       clearEntries();
     }
 }
+
+
+function deleteTrain(trainID) {
+  console.log(trainID);
+  var confirmDelete = confirm('Are you sure you want to delete this train?');
+
+  if (confirmDelete) {
+    console.log('deleting');
+    database.ref('trains/' + trainID).remove();
+
+  } else {
+    console.log('not deleting');
+  }
+}
+
+function getTrainData(trainID) {
+  database.ref('trains/' + trainID).once('value').then(function(snapshot) {
+    setModalValues(snapshot.val());
+  });
+}
+
+function setModalValues(snapshot) {
+  $('#modal-train-name').val(snapshot.trainName);
+  $('#modal-destination').val(snapshot.trainDestination);
+  $('#modal-first-start-time').val(snapshot.trainStartTime);
+  $('#modal-frequency').val(snapshot.trainFrequency);
+
+
+}
+
+
+function displayEditModal(trainID) {
+  var trainData = getTrainData(trainID);
+  editModal.css('display', 'block');
+  editModal.attr({
+    'data-train-id': trainID
+  });
+}
+
+//Close Modal on Button Click
+modalCloseBtn.on('click', function() {
+  editModal.css('display', 'none');
+});
+
+//Edit and Delete Button Click Listeners
+$('.panel-body').on('click', '.glyphicon', function(){
+  console.log(this.dataset.action);
+  if (this.dataset.action === 'delete') {
+    deleteTrain(this.id);
+
+  } else if (this.dataset.action === 'edit') {
+    displayEditModal(this.id);
+  }
+});
+
+
+function updateDB(trainID) {
+  console.log(trainID);
+  database.ref('trains/' + trainID).update({
+    trainName: $('#modal-train-name').val(),
+    trainDestination: $('#modal-destination').val(),
+    trainStartTime: $('#modal-first-start-time').val(),
+    trainFrequency: $('#modal-frequency').val()
+  });
+
+}
+
+
+//Edit modal Submit
+$('#modal-submit-btn').on('click', function() {
+
+  //Set Values
+  updateDB(editModal.data('train-id'));
+
+  //Close modal
+  editModal.css('display', 'none');
+
+});
